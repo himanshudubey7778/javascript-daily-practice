e237bec770a21a3f51cfa14886e4e7b9a9f3a62e
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-10-26 21:07:22 +0530

    update some code / add new features

--- JavaScript/3_conversion_Operation.js
+++ JavaScript/3_conversion_Operation.js
@@ -26,3 +26,39 @@ let SomeNumber = 33
 let stringNumber = String(SomeNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *********************** Operations ********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+let str1 = "Himanshu"
+let str2 = " Divyansh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
