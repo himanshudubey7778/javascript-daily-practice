639eed293648fa69e629157c5b35318c1dd67b69
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-10-26 17:43:43 +0530

    code add update

---
+++ JavaScript/2_DataTypes.js
@@ -0,0 +1,28 @@
+"Use Strict"; //Treat all JS code as newer version
+
+
+//alert(3 + 3) //We are using nodeJS, Not browser
+
+console.log(3 + 3)  //Code readability should be high
+
+console.log("himanshu")
+
+let name = "himanshu"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null standalone value
+//undefined => 
+//symbol => unique 
+
+//object 
+
+console.log(typeof "himanshu");
+console.log(typeof age);
+console.log(typeof null); //object 
+console.log(typeof undefined); //Undefined
