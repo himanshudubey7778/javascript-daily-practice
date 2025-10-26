e2045237090a878a615137a0a2cf66e968bd6b13
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-10-26 18:45:34 +0530

    conversion and operation

---
+++ JavaScript/3_conversion_Operation.js
@@ -0,0 +1,28 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+// 33 => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true , 0 => false
+//"" => false
+//"Himanshu" => true
+
+let SomeNumber = 33
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
