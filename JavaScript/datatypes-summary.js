5828055ef96b9f3e74e65cf63bdc50e4fb821ba9
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-10-28 01:58:09 +0530

    code add

---
+++ JavaScript/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive => call by value 
+// 7 types : string, Number, Boolean, null, undefined, Symbol,  BigINT
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTump = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id === anotherID);
+
+// const BigNumber = 372882728737383n
+
+
+
+// Non primitive (Call By refrence) 
+// Array, object, functions
+
+const heros = ["Shaktimaan" , "Spiderman" , "Bajrangi"]
+let myObj = {
+    name: "Himansu",
+    age: 18,
+
+}
+
+const myFunction = function(){
+    console.log("hello world!");
+}
+
+
+console.log(typeof scoreValue, score, isLoggedIn, outsideTump, );
