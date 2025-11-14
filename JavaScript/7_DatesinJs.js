eacec7c6e91b4672cdebcc226dd945f3dddf2867
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-11-15 01:58:54 +0530

    first commit add

---
+++ JavaScript/7_DatesinJs.js
@@ -0,0 +1,34 @@
+// Date
+
+let nowDate = new Date()
+// console.log(nowDate.toString());
+// console.log(nowDate.toDateString());
+// console.log(nowDate.toLocaleString());
+// console.log(typeof nowDate);
+
+// let nowCreatedDate = new Date(2023, 0, 23)
+// let nowCreatedDate = new Date(2023, 0, 23, 5, 9)
+// let nowCreatedDate = new Date("2023-02-14")
+let nowCreatedDate = new Date("01-14-2024")
+// console.log(nowCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(nowCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getHours());
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay() + 2);
+
+// `${newDate.getDay()} and the time`
+
+newDate.toLocaleDateString('default',{
+    weekday: "long",
+    timeZone: "now",
+}) // important case in jS
+
+
+
+
