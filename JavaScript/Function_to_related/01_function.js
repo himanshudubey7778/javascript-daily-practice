d2d8a52a149bb4b8c1d94d59046b3e1ddada57ce
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-11-21 08:50:15 +0530

    first commit add

---
+++ JavaScript/Function_to_related/01_function.js
@@ -0,0 +1,37 @@
+function sayMyName(){
+    console.log("h");
+    console.log("i");
+    console.log("m");
+    console.log("a");
+    console.log("n");
+    console.log("s");
+    console.log("h");
+    console.log("u");
+}
+
+// sayMyName()
+
+// function addTwoNumber(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumber(number1, number2){
+    console.log(number1 + number2);
+}
+
+// const result = addTwoNumber(3, 5)
+
+// console.log("Result", result);
+
+function loginUserMessage(username = "Sam"){
+    if(!username){
+        console.log("Please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("himanshu"));
+
+
+    
