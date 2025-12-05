61232f0b54288e9e8a2e267a79afdd947e8fd391
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-12-05 19:41:49 +0530

    first commit add

--- JavaScript/05-DEC/ToDo_app.js
+++ JavaScript/05-DEC/ToDo_app.js
@@ -1,78 +1,74 @@
-class Task {
-    constructor(id, desc = "No Description", isCompleted = false){
+class Task{
+    constructor(id,desc = "No Description",isCompleted = false){
         this.id = id;
         this.desc = desc;
         this.isCompleted = this.isCompleted;
     }
 }
 
-class TaskManager {
-    
+class TaskManager{
     constructor(){
-        this.task = this.readFromLocalStorage();
+        this.tasks = this.readFromLocalStorage();
     }
-    cerateTask(description){
-        const id = Date.now()
+    // add the task
+    createTask(description){
+        const id ="t" + Date.now().toString(36) + Math.random().toString(36).slice(2,9);
+        const task = new Task(id,description)
+        this.tasks.push(task)
+        this.savetoLocalStorage()
     }
-
-    //add the task 
-
-    createTask(description) {
-        const id = "t_" + Date.now().toString(36) + Math.random().toString(36).slice(2, 8);
-        const task = new Task(id, description);
-        this.tasks.push(task);
-        this.saveToLocalStorage();
+    
+    // delete the tasks
+    deleteTask(id){
+        this.tasks = this.tasks.filter(eTask => eTask.id !=id);
+        this.savetoLocalStorage();
     }
-    //delete the task 
-
-    deleteTask(id) {
-        this.tasks = this.tasks.filter(eTask => eTask.id != id);
-        this.saveToLocalStorage();
-    }
-    //updat the status of task
-
-    toggleStatus(id) {
-        const task = this.task.find(etask => eTask.id === id);
-        if(task) {
-            task.isCompleted = !task.isCompleted;
-            this.saveToLocalStorage();
+    // update the status of task
+    toogleStatus(id){
+        const task = this.tasks.find(eTask =>eTask.id===id);
+        if(task){
+            task.isCompleted = !task.isCompleted
         }
     }
-    //edit the task 
-
-    editTask(id, newDesk) {
-        const task = this.tasks.find(eTask => eTask.id === id);
-        if(task && newDesc?.length > 3) {
-            task.description = newDesc;
-            this.saveToLocalStorage();
+    // edit the task
+    editTask(id,desc){
+        const task = this.tasks.find(eTask => eTask.id==id);
+        if(task && desc.length>0){
+            task.desc = desc;
         }
     }
-    //store to local storage 
-
-    saveToLocalStorage() {
-        localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    }
-    //read from the local storage
-    readFromLocalStorage() {
-        return JSON.parse(localStorage.getItem('tasks')) || [];
-    }
-    //filter tasks based on all || completed || pending
-
-    filterTask(status) {
-        if(status === "pending")
-            return this.tasks.filter(eTask => eTask.isCompleted == false);
-        if(status === "completed")
-            return this.tasks.filter(etask => eTask.iscompleted === true);
-
+    // filter tasks based on status
+    filterTasks(status){
+        if(status==="pending"){
+            return this.tasks.filter(eTask => eTask.isCompleted===false);
+        }
+        if(status==="completed"){
+            return this.tasks.filter(eTask=>eTask.isCompleted===true)
+        }
         return this.tasks;
+    }
+    //  read from the local storage
+    readFromLocalStorage(){     
+        return JSON.parse(localStorage.getItem('tasks') || '[]');
+    }
+    // store to local storage
+    savetoLocalStorage(){
+        localStorage.setItem('tasks',JSON.stringify(this.tasks))
     }
 }
 
-//UiHandler
 
+
+// UI HANDLER
+
+
+// taskInput =
+// addBtn = document.getElementById("add-btn")
+// filters = document.getElementById("filters")
+// taskList = document.getElementById("task-list")
 class UIHandler{
    constructor(TaskManager){
-    this.TaskManager = TaskManager;
+    this.taskManager = TaskManager;
     this.taskInput =  document.getElementById("task-input");
     this.taskList = document.getElementById("task-list");
     this.filterButtons = document.querySelectorAll(".filter-btn");
@@ -84,11 +80,79 @@ class UIHandler{
     this.addBtn.addEventListener('click',()=>this.addTask())
     this.taskInput.addEventListener("keyup",(e)=>{
         if(e.key==="enter"){
-            
+            this.addTask()
         }
+    })
+    // this.filterButtons.forEach(btn=>{
+    //     btn.addEventListener("click",(e)=>{
+    //         this.filterOn = btn.dataset.filter
+    //     })
+    // })
+   }
+   addTask(){
+    const taskDesc = this.taskInput.value.trim();
+    console.log("add task called")
+    if(!taskDesc || taskDesc.length<3)
+        return alert("Please enter a valid task!")
+
+    this.taskManager.createTask(taskDesc);
+    this.taskInput.value = ""  
+    this.renderTasks();
+   }
+   renderTasks(){
+    this.taskList.innerHTML = "";
+    const tasks = this.taskManager.filterTasks(this.filterOn)
+    tasks.forEach(task=>{
+        const li = document.createElement('li');
+li.classList.add("task-item");
+if (task.isCompleted) {
+    li.classList.add("Completed");
+}
+
+        li.innerHTML = `
+                  <div class="task-left">
+            <input type="checkbox" ${task.isCompleted ? "checked" : ""} data-id="${task.id}" class="check">
+            <span>${task.desc}</span>
+          </div>
+          <div>
+            <button class="btn edit" data-id="${task.id}">Edit</button>
+            <button class="btn delete" data-id="${task.id}">Delete</button>
+          </div>
+        `
+        this.taskList.append(li)
+    })
+    this.dynamicEventListener();
+   }
+   dynamicEventListener(){
+    document.querySelectorAll('.check').forEach(eCheckBox=>{
+        eCheckBox.addEventListener('change',(e)=>{
+            this.taskManager.toogleStatus(e.target.dataset.id);
+            this.renderTasks();
+        })
+    });
+    document.querySelectorAll(".delete").forEach(deleteBtn=>{
+        deleteBtn.addEventListener("click",(e)=>{
+            this.taskManager.deleteTask(e.target.dataset.id);
+            this.renderTasks();
+        })
+    });
+    document.querySelectorAll(".edit").forEach(editBtn=>{
+        editBtn.addEventListener("click",(e)=>{
+            const id = e.target.dataset.id;
+            const newDesc = prompt("Edit Task: ");
+            if(newDesc){
+                this.taskManager.editTask(id,newDesc);
+                this.renderTasks();
+            } 
+        })
     })
    }
 }
 
 const app = new UIHandler(new TaskManager())
 
+// filters.forEach(btn=>{
+//     btn.addEventListner("click",()+>{
+//         console.log(btn.dataset.filter)
+//     })
+// })
---
+++ JavaScript/To-Do_project/05_Dec/Todo/practice.js
@@ -0,0 +1,6 @@
+//Slect Dom Elements
+const input = document.getElementById('todo-input')
+const addBtn = document.getElementById('add-btn')
+const list = document.getElementById('todo-list')
+
+
